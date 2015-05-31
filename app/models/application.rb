010c872fe5d8de0f8c600e8af18c996a36ae8a8e
badsauce <ninjazoby@gmail.com>
2015-05-31 17:03:38 -0500

    patched email validator

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,5 @@
 class Application < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, email: true
   validates :school, presence: true
 end
