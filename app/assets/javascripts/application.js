7bfd1b63baa0cf59312db638166d04d36eb10ad4
badsauce <nickrutowski@gmail.com>
2015-07-02 12:31:22 -0500

    removed trubolinks artifact

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,16 +12,15 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require flipclock.min
 //= require_tree .
 
 $(document).on('ready on load', function() {
-    
+
 //    var date  = new Date(Date.new(2015, 9, 25, 12, 0, 0));
 //    var now   = new Date();
 //    var diff  = date.getTime()/1000 - now.getTime()/1000;
-    
+
     var clock = $('#countdown_clock').FlipClock({
         clockFace: 'DailyCounter',
 		countdown: true,
