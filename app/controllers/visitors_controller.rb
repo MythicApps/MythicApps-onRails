89650a9edfa1e5f83bc498ea5312e2c8141250a3
badsauce <nickrutowski@gmail.com>
2015-07-07 23:32:23 -0500

    logged in attendees see edit application button

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,14 @@
 class VisitorsController < ApplicationController
   def index
-    @application = Application.new
     @omit_home = true
+    if user_signed_in?
+      @application = Application.find_by email: current_user.email
+      if  @application.blank?
+        @application = Application.new
+      end
+    else
+      @application = Application.new
+    end
   end
 
   def countdown
