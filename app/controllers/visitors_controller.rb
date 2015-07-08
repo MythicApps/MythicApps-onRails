3de183a8ea34941dd40283613a50ab54467d46d9
badsauce <nickrutowski@gmail.com>
2015-07-07 22:56:38 -0500

    improved navigation back to home

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,9 +1,11 @@
 class VisitorsController < ApplicationController
   def index
     @application = Application.new
+    @omit_home = true
   end
-    
-    def countdown
-    end
+
+  def countdown
+
+  end
 
 end
